a71dcc25ddc5cc0e98b1f710aada242bc7044faf
Nolan McCafferty <nolanmccafferty@gmail.com>
2024-08-27 15:38:22 -0500

    Fix testing for new syntax

--- test.py
+++ test.py
@@ -1,12 +1,13 @@
 import requests
 
-from oqo_validate.validate import validate_oqo
+from oqo_validate import OQOValidator
 
 r = requests.get(
     'https://raw.githubusercontent.com/ourresearch/oqo-search-tests/main/tests.json')
 r.raise_for_status()
 tests = r.json()
+validator = OQOValidator()
 for test in tests:
-    ok, error = validate_oqo(test['query'])
+    ok, error = validator.validate(test['query'])
     if not ok:
         print((ok, error))
