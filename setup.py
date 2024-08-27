757c9bef4b6becdf17a8ce4889ad465ec7d304ff
Nolan McCafferty <nolanmccafferty@gmail.com>
2024-08-27 11:56:38 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='oqo-validate',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='nolanmccafferty',
+    author_email='',
+    description=''
+)
