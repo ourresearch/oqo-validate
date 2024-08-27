a00573609e52c95391fb03db3c4728b1f875584c
Nolan McCafferty <nolanmccafferty@gmail.com>
2024-08-27 15:07:14 -0500

    Add requests dependency

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='oqo-validate',
     version='1.0',
-    packages=['oqo_validate'],
+    packages=find_packages(),
+    install_requires=['requests'],
     url='',
     license='',
     author='nolanmccafferty',
