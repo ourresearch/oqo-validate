59749e93423a2146cdde9319bac8becf4196c097
Nolan McCafferty <nolanmccafferty@gmail.com>
2024-09-04 11:10:36 -0500

    Update for new schema, yaml tests

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='oqo-validate',
     version='1.0',
     packages=find_packages(),
-    install_requires=['requests'],
+    install_requires=['requests', 'pyyaml'],
     url='',
     license='',
     author='nolanmccafferty',
--- test.py
+++ test.py
@@ -1,11 +1,13 @@
 import requests
+import yaml
 
 from oqo_validate import OQOValidator
 
 r = requests.get(
-    'https://raw.githubusercontent.com/ourresearch/oqo-search-tests/main/tests.json')
+    'https://raw.githubusercontent.com/ourresearch/oqo-search-tests/b484a7b0e7bf90ed47c06c3d81567deb26c99bce/new_tests.yaml')
 r.raise_for_status()
-tests = r.json()
+tests = yaml.safe_load(r.content)
+
 validator = OQOValidator()
 for test in tests:
     ok, error = validator.validate(test['query'])
